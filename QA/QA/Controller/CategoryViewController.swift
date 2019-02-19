ee7557815534d62d47ae3e6cba74c78eda676bb0
Nurzhigit Smailov <nusmailov@gmail.com>
2019-02-20 00:33:42 +0600

    InformationByCategory

--- QA/QA/Controller/CategoryViewController.swift
+++ QA/QA/Controller/CategoryViewController.swift
@@ -9,9 +9,56 @@
 import UIKit
 
 class CategoryViewController: UIViewController {
+    var informationEducation: Question?
+    var informationSport: Question?
+    var informationHistory: Question?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadInformation(number: 9)
+        loadInformation(number: 21)
+        loadInformation(number: 23)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "Education"{
+            if let cvc = segue.destination as? QuestionViewController{
+                loadInformation(number: 9)
+                cvc.information = self.informationEducation
+            }
+        }else if segue.identifier == "Sport"{
+            if let cvc = segue.destination as? QuestionViewController{
+                loadInformation(number: 21)
+                cvc.information = self.informationSport
+            }
+        }else if segue.identifier == "History"{
+            if let cvc = segue.destination as? QuestionViewController{
+                loadInformation(number: 23)
+                cvc.information = self.informationHistory
+            }
+        }
+    }
+    
+    
+    func loadInformation(number: Int){
+        let jsonUrl = "https://opentdb.com/api.php?amount=20&category=\(number)&difficulty=medium&type=multiple"
+        guard let url = URL(string: jsonUrl) else {return }
+        URLSession.shared.dataTask(with: url){(data, response, error) in
+            guard let data = data else{return }
+            do{
+                let jsonHolder = try JSONDecoder().decode(Question.self, from: data)
+                if(number == 9){
+                    self.informationEducation = jsonHolder
+                }else if(number == 21){
+                    self.informationSport = jsonHolder
+                }else{
+                    self.informationHistory = jsonHolder
+                }
+            }catch let error{
+                print("Error\n",error)
+            }
+            }.resume()
     }
     
 }
--- QA/QA/Controller/QuestionViewController.swift
+++ QA/QA/Controller/QuestionViewController.swift
@@ -9,24 +9,15 @@
 import UIKit
 
 class QuestionViewController: UIViewController {
-    var information: Question?
+    var information: Question!
+    var cnt = 0;
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
-        loadInformation();
+        
     }
-    func loadInformation(){
-        let jsonUrl = "https://opentdb.com/api.php?amount=20&category=9&difficulty=medium&type=multiple"
-        guard let url = URL(string: jsonUrl) else {return }
-        URLSession.shared.dataTask(with: url){(data, response, error) in
-        guard let data = data else{return }
-            do{
-                let jsonHolder = try JSONDecoder().decode(Question.self, from: data)
-                self.information = jsonHolder
-            }catch let error{
-                print("Error\n",error)
-            }
-        }.resume()
-    }
+    
+    
+    
     
 }
