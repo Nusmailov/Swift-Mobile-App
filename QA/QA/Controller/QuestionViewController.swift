331532eee3b2205eaa477379fa26c5350dc56fdd
Nurzhigit Smailov <nusmailov@gmail.com>
2019-02-20 23:59:42 +0600

    QuestionModelDone

--- QA/QA/Controller/QuestionViewController.swift
+++ QA/QA/Controller/QuestionViewController.swift
@@ -17,58 +17,59 @@ class QuestionViewController: UIViewController {
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet weak var questionNumber: UILabel!
     
-    @IBAction func touch1(_ sender: UIButton) {
-        if sender.titleLabel?.text == information!.results?[cnt].correctAnswer{
-            rightAnswer += 1
+    @IBAction func touchButtons(_ sender: UIButton) {
+        print(cnt)
+        if(self.cnt <= 20){
+            if sender.titleLabel?.text == information!.results?[cnt-1].correctAnswer{
+                rightAnswer += 1
+                sender.backgroundColor = UIColor.green
+            }else{
+                sender.backgroundColor = UIColor.red
+            }
+            for i in buttons{
+                if i.titleLabel?.text == information!.results?[cnt-1].correctAnswer{
+                    i.backgroundColor  = UIColor.green
+                }
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0) {
+                for i in self.buttons{
+                    i.backgroundColor  = UIColor.black
+                }
+                self.reloadQuestion()
+            }
+        }else{
+            finishTap();
         }
-        reloadQuestion()
-    }
-    
-    @IBAction func touch2(_ sender: UIButton) {
-        if sender.titleLabel?.text == information!.results?[cnt].correctAnswer{
-            rightAnswer += 1
-        }
-        reloadQuestion()
-    }
-    
-    @IBAction func touch3(_ sender: UIButton) {
-        if sender.titleLabel?.text == information!.results?[cnt].correctAnswer{
-            rightAnswer += 1
-        }
-        reloadQuestion()
-    }
-    
-    @IBAction func touch4(_ sender: UIButton) {
-        if sender.titleLabel?.text == information!.results?[cnt].correctAnswer{
-            rightAnswer += 1
-        }
-        reloadQuestion()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
-        //print(self.information)
+        let finishButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(finishTap))
+        self.navigationItem.rightBarButtonItem = finishButton
         reloadQuestion()
     }
-    
+    @objc func finishTap(){
+        
+        let homeView = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as! ResultViewController
+        present(homeView, animated: true, completion: nil)
+    }
     
     func reloadQuestion(){
         var variants = [String]()
         variants.removeAll()
-        for i in (information!.results?[cnt].incorrectAnswers)!{
-            variants.append(i)
-        }
-        variants.append((information!.results?[cnt].correctAnswer)!)
-        variants.shuffle()
-        for i in 0..<buttons.count{
-            buttons[i].setTitle(variants[i], for: .normal)
-        }
-        questionText.text = information!.results?[cnt].question
-        if(cnt  <= 20){
+        if(cnt < 20){
+            for i in (information!.results?[cnt].incorrectAnswers)!{
+                variants.append(i)
+            }
+            variants.append((information!.results?[cnt].correctAnswer)!)
+            variants.shuffle()
+            for i in 0..<buttons.count{
+                buttons[i].setTitle(variants[i], for: .normal)
+            }
+            questionText.text = information!.results?[cnt].question
             cnt+=1
         }
         questionNumber.text = "\(cnt)/20"
     }
-    
 }
--- QA/QA/Controller/ResultViewController.swift
+++ QA/QA/Controller/ResultViewController.swift
@@ -1,17 +1,16 @@
 //
-//  ViewController.swift
+//  ResultViewController.swift
 //  QA
 //
-//  Created by Nurzhigit Smailov on 2/15/19.
+//  Created by Nurzhigit Smailov on 2/20/19.
 //  Copyright © 2019 Nurzhigit Smailov. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ResultViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 }
-
